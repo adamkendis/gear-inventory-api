328c5b3e84e490dc2a14481ef59e6fa45f7d3a71
Adam Kendis <atkendis@gmail.com>
2019-04-27 01:46:35 -0700

    Added create item feature.

--- app/gear/tests/test_items_api.py
+++ app/gear/tests/test_items_api.py
@@ -80,3 +80,24 @@ class PrivateItemsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], item.name)
+
+    def test_create_item_successful(self):
+        """Test creating a new item"""
+        payload = {'name': 'test backpack',
+                   'notes': 'test notes',
+                   'weight': 200.50}
+        self.client.post(ITEMS_URL, payload)
+
+        exists = Item.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_item_invalid(self):
+        """Test creating a new item with invalid payload"""
+        payload = {'name': '', 'notes': '', 'weight': 0}
+        res = self.client.post(ITEMS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
